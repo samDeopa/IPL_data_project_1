8c9f6fa785c587f42dc78d6088f4d5738287d911
Sameer Chand <sam@manishas-MacBook-Air.local>
2025-02-06 12:13:23 +0530

    added csv to json convertor

---
+++ src/server/csvToJson.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const CsvToJson = (csvPath) => {
+  const data = fs.readFileSync(csvPath, "utf-8").replace(/\r/g, "").split("\n");
+
+  const columns = data[0].split(",");
+
+  data.splice(0, 1);
+  const jsonData = [];
+  data.forEach((row) => {
+    const entry = {};
+    row.split(",").forEach((item, index) => {
+      entry[columns[index]] = item;
+    });
+    jsonData.push(entry);
+  });
+  return jsonData;
+};
+console.log(CsvToJson("../data/matches.csv"));
